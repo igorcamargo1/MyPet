092b375fc1379af03d9f0fa0881cb9f319950cdd
Igor Gabriel de Camargo <rm94553@fiap.com.br>
2023-05-22 21:42:23 -0300

    jwt

--- src/main/java/br/com/fiap/mypet/models/Usuario.java
+++ src/main/java/br/com/fiap/mypet/models/Usuario.java
@@ -1,8 +1,11 @@
 package br.com.fiap.mypet.models;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,7 +25,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,6 +45,49 @@ public class Usuario {
 
     @NotBlank @Size(min = 6, message = "A senha deve conter pelo menos 6 digitos")
     private String senha;
+    
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USUARIO"));
+    }
+
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     
 
 }
